b5e5b229b1d2e569c44268a4a3dae8eb1c1c7a5b
Meme Houeibib <38425746+memehoueibib@users.noreply.github.com>
2023-11-21 15:06:24 +0100

    Ajout d'user model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    # champs supplémentaires ici si nécessaire
+    bio = models.TextField("Biographie", blank=True)
+    birth_date = models.DateField("Date de naissance", null=True, blank=True)
+    # d'autres champs personnalisés ici
+
+    def __str__(self):
+        return self.username
