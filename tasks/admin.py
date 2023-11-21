c09af58e4334167a4823fa9fb4afd024ed6845df
Meme Houeibib <38425746+memehoueibib@users.noreply.github.com>
2023-11-21 15:16:35 +0100

    Enregistrement dans  l'admin
    Pour pouvoir gérer les utilisateurs personnalisés dans l'interface d'administration de Django.
    &
    Enregistrement du modèle Task dans l'interface d'administration de Django

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Task, Comment
 
-# Register your models here.
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'priority', 'due_date', 'assigned_to')
+    list_filter = ('status', 'priority', 'due_date', 'assigned_to')
+    search_fields = ('title', 'description')
+    inlines = [CommentInline]
+
+admin.site.register(Task, TaskAdmin)
--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+# users/admin.py
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+
+admin.site.register(CustomUser, UserAdmin)
